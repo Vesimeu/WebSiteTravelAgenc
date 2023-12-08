f5e12b8b0cd339f1c8d4dd738581643161c0a649
ANANAzZzZz <vladscooltrol@gmail.com>
2023-12-08 19:18:50 +0300

    add some basic templates and routes for them

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,36 @@
 from app import app
+import psycopg
+from flask import render_template
 
-@app.route('/')
-def hello_world():
-    return "Hello, World!"
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/testdb')
+def test_connection():
+    try:
+        con = psycopg.connect(host=app.config['DB_SERVER'],
+                              user=app.config['DB_USER'],
+                              password=app.config['DB_PASSWORD'],
+                              dbname=app.config['DB_NAME'])
+    except Exception as e:
+        message = f"Connection error: {e}"
+        return message
+
+    cur = con.cursor()
+    cur.execute("SELECT * FROM HOTEL")
+    message = cur.fetchall()
+
+    return message
+
+@app.route('/hotels')
+def show_clients():
+    with psycopg.connect(host=app.config['DB_SERVER'],
+                         user=app.config['DB_USER'],
+                         password=app.config['DB_PASSWORD'],
+                         dbname=app.config['DB_NAME']) as con:
+
+        cur = con.cursor()
+        hotel_names = cur.execute(f'SELECT name FROM hotel').fetchall()
+
+        return render_template('hotels.html', title="Отели", hotel_names=hotel_names)
--- config.py
+++ config.py
@@ -7,3 +7,7 @@ load_dotenv()
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'abcde'
     DB_SERVER = os.environ.get('DB_SERVER') or 'localhost'
+    DB_USER = os.environ.get('DB_USER') or 'user'
+    DB_NAME = os.environ.get('DB_NAME') or 'db'
+    DB_PASSWORD = os.environ.get('DB_PASSWORD') or 'user'
+
